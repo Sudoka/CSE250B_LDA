62ad6be6cba8aa84071415fbc87e3025e1f25a8b
David Larson <dplarson@ucsd.edu>
2013-02-27 12:45:31 -0800

    Add simplex plots.

--- simplex_plot.py
+++ simplex_plot.py
@@ -8,6 +8,7 @@ source: https://gist.github.com/davidandrzej/939840
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as lines
+import scipy.io as sio
 
 
 def projectSimplex(data):
@@ -79,7 +80,7 @@ def plotSimplex(topic1, topic2, topic3, grayscale=True):
     ax = plt.subplot(111)
 
     # plot data
-    kwargs = {'s': 70}
+    kwargs = {'s': 50}
 
     if grayscale is False:
         colors = ['#dc322f', '#000000', '#268bd2']
@@ -89,12 +90,12 @@ def plotSimplex(topic1, topic2, topic3, grayscale=True):
     ax.scatter(simplex1[:, 0], simplex1[:, 1], label='Topic 1',
                marker='o', color=colors[0], **kwargs)
     ax.scatter(simplex2[:, 0], simplex2[:, 1], label='Topic 2',
-               marker='x', color=colors[1], **kwargs)
+               marker='s', color=colors[1], **kwargs)
     ax.scatter(simplex3[:, 0], simplex3[:, 1], label='Topic 3',
                marker='^', color=colors[2], **kwargs)
 
     # add legend
-    ax.legend(frameon=True)
+    #ax.legend()
 
     # add triangle outline
     lines_triangle = lines.Line2D([0.0, 0.5, 1.0, 0.0],
@@ -115,23 +116,94 @@ def plotSimplex(topic1, topic2, topic3, grayscale=True):
     ax.set_xlim([-0.2, 1.2])
     ax.set_ylim([-0.2, 1.2])
 
-    #plt.savefig('topic_distribution.pdf')
-    plt.show()
+    plt.savefig('theta_classic400_t1circle_t2square_t3_diamond.pdf')
+    #plt.show()
+
+
+def plotSimplexSingle(topic, grayscale=True):
+    """
+    Plot 3D data onto 2D triangular simplex.
+
+    Parameters
+    ----------
+    topic : (N, 3) array_like
+    grayscale : bool
+        Whether to show plot in grayscale (True) or color (False).
+
+    """
+
+    # test data
+    simplex = projectSimplex(topic)
+
+    # plot
+    plt.figure(facecolor='w')
+    ax = plt.subplot(111)
+
+    # plot data
+    kwargs = {'s': 50}
+
+    if grayscale is False:
+        colors = ['#dc322f', '#000000', '#268bd2']
+    else:
+        colors = ['#000000', '#000000', '#999999']
+
+    ax.scatter(simplex[:, 0], simplex[:, 1], label='Topic 1',
+               marker='o', color=colors[0], **kwargs)
+
+    # add legend
+    #ax.legend()
+
+    # add triangle outline
+    lines_triangle = lines.Line2D([0.0, 0.5, 1.0, 0.0],
+                                  [0.0, np.sqrt(3) / 2, 0.0, 0.0],
+                                  color='k')
+    ax.add_line(lines_triangle)
+
+    # add labels to triange vertex
+    text_kwargs = {'fontsize': 14}
+    ax.text(-0.05, -0.05, r'$\theta_1$', **text_kwargs)
+    ax.text(1.05, -0.05, r'$\theta_2$', **text_kwargs)
+    ax.text(0.50, np.sqrt(3) / 2 + 0.05, r'$\theta_3$', **text_kwargs)
+
+    # remove x-y axis
+    ax.axis('off')
+
+    # set limits
+    ax.set_xlim([-0.2, 1.2])
+    ax.set_ylim([-0.2, 1.2])
+
+    plt.savefig('thetas_kos_topic.pdf')
+    #plt.show()
+
+#
+# Classic400
+#
+
+# get labels
+data_classic400 = sio.loadmat('classic400/classic400.mat')
+labels_classic400 = data_classic400['truelabels']
+
+
+# get theta values
+thetas_classic400 = np.genfromtxt('classic400/classic400_thetas.csv',
+                                  delimiter=',')
+thetas_classic400 = np.exp(thetas_classic400)
+
+# split thetas into topics (NOTE: true labels must be known)
+thetas_topic1 = thetas_classic400[0:99]
+thetas_topic2 = thetas_classic400[100:199]
+thetas_topic3 = thetas_classic400[200:]
+
+plotSimplex(thetas_topic1, thetas_topic2, thetas_topic3, grayscale=False)
 
 
 #
-# Demo simplex plotting
+# KOS
 #
 
-# create demo data (Nx3 arrays)
-topic1 = np.array([[0.9, 0.0, 0.0],
-                   [0.6, 0.2, 0.1],
-                   [0.7, 0.1, 0.2]])
-topic2 = np.array([[0.2, 0.9, 0.1],
-                   [0.0, 0.7, 0.0],
-                   [0.1, 0.8, 0.2]])
-topic3 = np.array([[0.1, 0.2, 0.8],
-                   [0.0, 0.0, 0.6],
-                   [0.2, 0.1, 0.9]])
+# get theta values
+thetas_kos = np.genfromtxt('KOS400/KOS400_thetas.csv',
+                           delimiter=',')
+thetas_kos = np.exp(thetas_kos)
 
-plotSimplex(topic1, topic2, topic3, grayscale=False)
+plotSimplexSingle(thetas_kos, grayscale=False)
