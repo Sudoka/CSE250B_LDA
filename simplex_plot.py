571e301b44a64eaf7b15003661eda2f809bbd65c
David Larson <dplarson@ucsd.edu>
2013-02-27 17:58:32 -0800

    Add plots for second alpha/beta combo.

--- simplex_plot.py
+++ simplex_plot.py
@@ -8,7 +8,6 @@ source: https://gist.github.com/davidandrzej/939840
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as lines
-import scipy.io as sio
 
 
 def projectSimplex(data):
@@ -56,46 +55,22 @@ def projectSimplex(data):
     return points
 
 
-def plotSimplex(topic1, topic2, topic3, grayscale=True):
+def plotSimplex(thetas, dataset, grayscale=True, save=False, show=True):
     """
     Plot 3D data onto 2D triangular simplex.
 
     Parameters
     ----------
-    topic1 : (N, 3) array_like
-    topic2 : (N, 3) array_like
-    topic3 : (N, 3) array_like
+    thetas : (N, 3) array_like
+    dataset : str
+        Which dataset are the theta values calculated from.
     grayscale : bool
         Whether to show plot in grayscale (True) or color (False).
 
     """
 
-    # test data
-    simplex1 = projectSimplex(topic1)
-    simplex2 = projectSimplex(topic2)
-    simplex3 = projectSimplex(topic3)
-
-    # plot
     plt.figure(facecolor='w')
     ax = plt.subplot(111)
-
-    # plot data
-    kwargs = {'s': 50}
-
-    if grayscale is False:
-        colors = ['#dc322f', '#000000', '#268bd2']
-    else:
-        colors = ['#000000', '#000000', '#999999']
-
-    ax.scatter(simplex1[:, 0], simplex1[:, 1], label='Topic 1',
-               marker='o', color=colors[0], **kwargs)
-    ax.scatter(simplex2[:, 0], simplex2[:, 1], label='Topic 2',
-               marker='s', color=colors[1], **kwargs)
-    ax.scatter(simplex3[:, 0], simplex3[:, 1], label='Topic 3',
-               marker='^', color=colors[2], **kwargs)
-
-    # add legend
-    #ax.legend()
 
     # add triangle outline
     lines_triangle = lines.Line2D([0.0, 0.5, 1.0, 0.0],
@@ -116,94 +91,76 @@ def plotSimplex(topic1, topic2, topic3, grayscale=True):
     ax.set_xlim([-0.2, 1.2])
     ax.set_ylim([-0.2, 1.2])
 
-    plt.savefig('theta_classic400_t1circle_t2square_t3_diamond.pdf')
-    #plt.show()
-
-
-def plotSimplexSingle(topic, grayscale=True):
-    """
-    Plot 3D data onto 2D triangular simplex.
-
-    Parameters
-    ----------
-    topic : (N, 3) array_like
-    grayscale : bool
-        Whether to show plot in grayscale (True) or color (False).
-
-    """
-
-    # test data
-    simplex = projectSimplex(topic)
-
-    # plot
-    plt.figure(facecolor='w')
-    ax = plt.subplot(111)
-
-    # plot data
-    kwargs = {'s': 50}
-
+    # plot formatting
+    kwargs = {'s': 60}
+    markers = ['o', 's', '^']
     if grayscale is False:
-        colors = ['#dc322f', '#000000', '#268bd2']
+        colors = ['#268bd2', '#000000', '#dc322f']
     else:
         colors = ['#000000', '#000000', '#999999']
 
-    ax.scatter(simplex[:, 0], simplex[:, 1], label='Topic 1',
-               marker='o', color=colors[0], **kwargs)
+    # plot data
+    if dataset is 'classic400':
+        # split data by topic
+        topic1 = thetas[0:99, :]
+        topic2 = thetas[100:199, :]
+        topic3 = thetas[200:, :]
 
-    # add legend
-    #ax.legend()
+        # convert data to simplex coordinates
+        simplex1 = projectSimplex(topic1)
+        simplex2 = projectSimplex(topic2)
+        simplex3 = projectSimplex(topic3)
 
-    # add triangle outline
-    lines_triangle = lines.Line2D([0.0, 0.5, 1.0, 0.0],
-                                  [0.0, np.sqrt(3) / 2, 0.0, 0.0],
-                                  color='k')
-    ax.add_line(lines_triangle)
+        # plot data onto simplex
+        ax.scatter(simplex1[:, 0], simplex1[:, 1], label='Topic 1',
+                   marker=markers[0], color=colors[0], **kwargs)
+        ax.scatter(simplex2[:, 0], simplex2[:, 1], label='Topic 2',
+                   marker=markers[1], color=colors[1], facecolors='w', **kwargs)
+        ax.scatter(simplex3[:, 0], simplex3[:, 1], label='Topic 3',
+                   marker=markers[2], color=colors[2], **kwargs)
 
-    # add labels to triange vertex
-    text_kwargs = {'fontsize': 14}
-    ax.text(-0.05, -0.05, r'$\theta_1$', **text_kwargs)
-    ax.text(1.05, -0.05, r'$\theta_2$', **text_kwargs)
-    ax.text(0.50, np.sqrt(3) / 2 + 0.05, r'$\theta_3$', **text_kwargs)
+        ax.legend()
 
-    # remove x-y axis
-    ax.axis('off')
+        plot_name = 'classic400_thetas.pdf'
 
-    # set limits
-    ax.set_xlim([-0.2, 1.2])
-    ax.set_ylim([-0.2, 1.2])
+    elif dataset is 'KOS':
+        # convert data to simplex coordinates
+        simplex1 = projectSimplex(thetas)
 
-    plt.savefig('thetas_kos_topic.pdf')
-    #plt.show()
+        # plot data onto simplex
+        ax.scatter(simplex1[:, 0], simplex1[:, 1], label='Topic 1',
+                   marker=markers[0], color=colors[0], **kwargs)
+
+        plot_name = 'kos_thetas.pdf'
+
+    else:
+        print "Error: incorrect dataset given."
+
+    if save is True:
+        plt.savefig(plot_name)
+
+    if show is True:
+        plt.show()
+
 
 #
 # Classic400
 #
-
-# get labels
-data_classic400 = sio.loadmat('classic400/classic400.mat')
-labels_classic400 = data_classic400['truelabels']
-
-
-# get theta values
-thetas_classic400 = np.genfromtxt('classic400/classic400_thetas.csv',
+thetas_classic400 = np.genfromtxt('classic400/classic400_thetas_2.csv',
                                   delimiter=',')
+
+# convert from log-10
 thetas_classic400 = np.exp(thetas_classic400)
 
-# split thetas into topics (NOTE: true labels must be known)
-thetas_topic1 = thetas_classic400[0:99]
-thetas_topic2 = thetas_classic400[100:199]
-thetas_topic3 = thetas_classic400[200:]
-
-plotSimplex(thetas_topic1, thetas_topic2, thetas_topic3, grayscale=False)
+plotSimplex(thetas_classic400, 'classic400', grayscale=False)
 
 
 #
 # KOS
 #
+thetas_kos = np.genfromtxt('KOS400/KOS400_thetas.csv', delimiter=',')
 
-# get theta values
-thetas_kos = np.genfromtxt('KOS400/KOS400_thetas.csv',
-                           delimiter=',')
+# convert from log-10
 thetas_kos = np.exp(thetas_kos)
 
-plotSimplexSingle(thetas_kos, grayscale=False)
+plotSimplex(thetas_kos, 'KOS', grayscale=False)
